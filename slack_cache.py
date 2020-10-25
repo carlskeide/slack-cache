e04b129ae45a4033142c34e0c7a4787ac0c9bda2
Carl Skeide <carl@skeide.se>
2020-10-25 13:13:46 +0100

    Replace deprecated user information gathering

--- slack_cache.py
+++ slack_cache.py
@@ -15,10 +15,8 @@ class SlackError(Exception):
 
 class CachedSlack(object):
     ttl = {
-        "users": 60 * 60 * 24 * 5,
-        "profile": 60 * 60 * 24 * 5,
-        "channel": 60 * 60 * 16,
-        "presence": 60 * 20,
+        "profile": 60 * 60,
+        "channel": 60 * 60
     }
 
     def __init__(
@@ -50,62 +48,8 @@ class CachedSlack(object):
 
             return response
 
-    def is_present(self, user_id: str) -> bool:
-        """ Check whether a given USERID is marked as present """
-
-        logger.debug("Checking presence for: %s", user_id)
-
-        presence_key = self._cache_key('PRESENCE')
-
-        cached_presence = self.db.hget(presence_key, user_id)
-        if cached_presence:
-            return (cached_presence == "active")
-
-        logger.debug("Refreshing presence")
-        response = self._slack("users.list", presence=True)
-
-        user_presence = {u["id"]: u.get("presence", "away")
-                         for u in response["members"]}
-
-        self.db.hmset(presence_key, user_presence)
-        self.db.expire(presence_key, self.ttl["presence"])
-
-        return (user_presence.get(user_id) == "active")
-
-    def user_name(self, user_id: str) -> str:
-        """ Get the current username for USERID """
-
-        logger.debug("Fetching user: %s", user_id)
-
-        users_key = self._cache_key('USERS')
-
-        cached_user = self.db.hget(users_key, user_id)
-        if cached_user:
-            return (cached_user
-                if not self.db.sismember(self.ignored_key, user_id) else None)
-
-        logger.info("Refreshing userlist")
-        response = self._slack("users.list")
-
-        all_users = {u["id"]: u["name"]
-                     for u in response["members"]}
-
-        self.db.hmset(users_key, all_users)
-        self.db.expire(users_key, self.ttl["users"])
-
-        ignored_users = [u["id"] for u in response["members"]
-                         if (u["deleted"] or u.get("is_bot"))]
-
-        ignored_users.append("slackbot")  # Slackbot, is_bot == False ... Wat?
-
-        self.db.sadd(self.ignored_key, *ignored_users)
-        self.db.expire(self.ignored_key, self.ttl["users"])
-
-        return (all_users.get(user_id)
-            if user_id not in ignored_users else None)
-
-    def profile(self, user_id: str) -> dict:
-        """ Fetch a slack user profile """
+    def _get_profile(self, user_id: str) -> dict:
+        """ Fetch a user profile """
         logger.debug("Fetching profile: %s", user_id)
 
         profile_key = self._cache_key('PROFILE', str(user_id))
@@ -116,24 +60,30 @@ class CachedSlack(object):
 
         logger.info("Refreshing profile: %s", user_id)
         response = self._slack("users.info", user=user_id)
-
         profile = response["user"]["profile"]
-        profile.update({})
+
         self.db.hmset(profile_key, profile)
         self.db.expire(profile_key, self.ttl["profile"])
 
         return profile
 
     def avatar(self, user_id: str, size: int = 192) -> str:
+        """ Fetch a user's avatar URL """
         logger.debug(u"Fetching avatar for user: %s", user_id)
 
-        profile = self.profile(user_id)
+        profile = self._get_profile(user_id)
         image_key = "image_{}".format(size)
-
         return profile[image_key]
 
+    def user_name(self, user_id: str, real_name: bool = False) -> str:
+        """ Fetch a user's name """
+        logger.debug(u"Fetching name for user: %s", user_id)
+
+        profile = self._get_profile(user_id)
+        return profile["real_name" if real_name else "display_name"]
+
     def channel_members(self, channel_id: str) -> list:
-        """ Fetch all memebers of a channel """
+        """ Fetch all members of a channel """
         logger.debug("Fetching channel: {}".format(channel_id))
 
         channel_key = self._cache_key('CHANNEL', str(channel_id))
